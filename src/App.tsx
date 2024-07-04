695dc55a1710e4c2a031cf43df49f7e6b0f04cb5
SergeiVS <fam.smirnoff@gmail.com>
2024-07-04 21:48:40 +0200

    change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 // import Lesson06 from "./lessons/Lesson06/Lesson06";
-import Homework06 from "./homeworks/Homework06/Homeworn06";
+import Homework06 from "./homeworks/Homework06/Homework06";
 
 function App() {
   return (
