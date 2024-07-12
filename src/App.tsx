8940d308da4e1bee17e11da609e364c7230b93ce
SergeiVS <fam.smirnoff@gmail.com>
2024-07-12 21:28:43 +0200

    add Leson10

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GlobalStyles from "styles/GlobalStyles";
 // import Lesson07 from "lessons/Lesson07/Lesson07";
 import Lesson08 from "lessons/Lesson08/Lesson08";
 import Lesson09 from "lessons/Lesson09/Lesson09";
+import Lesson10 from "lessons/Lesson10/Lesson10";
 
 // import Homework06 from "homeworks/Homework06/Homework06";
 import Homework07 from "homeworks/Homework07/Homework07";
@@ -20,6 +21,7 @@ function App() {
       {/* <Lesson07 /> */}
       {/* <Lesson08 /> */}
       {/* <Lesson09 /> */}
+      <Lesson10 />
       {/* <Homework06 /> */}
       {/* <Homework07 /> */}
       {/* <Homework08 /> */}
---
+++ src/lessons/Lesson10/styles.ts
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Lesson_10Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  gap: 40px;
+  align-items: center;
+  padding: 40px;
+`;
