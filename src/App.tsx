62f6421e99779a8ddbd91964200729a7977ea143
ValeriaKornienko <valebog94@gmail.com>
2024-07-13 12:42:02 +0200

    Project styles, commit ba Valeria

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Homework08 from "homeworks/Homework08/Homework08";
 import Homework09 from "homeworks/Homework09/Homework09";
 
 import Consultation03 from "consultations/Consultation03/Consultation03";
+import Lesson10 from "lessons/Lesson10/Lesson10";
 
 function App() {
   return (
@@ -23,8 +24,9 @@ function App() {
       {/* <Homework06 /> */}
       {/* <Homework07 /> */}
       {/* <Homework08 /> */}
-      <Homework09 />
+      {/*<Homework09 />*/}
       {/* <Consultation03 /> */}
+      <Lesson10 />
     </>
   );
 }
---
+++ src/lessons/Lesson10/Lesson10.tsx
@@ -0,0 +1,12 @@
+import { Lesson_10Div, Lesson_10Header, Lesson_10Cards } from "./styles";
+
+function Lesson10(){
+    return(
+        <Lesson_10Div>
+            <Lesson_10Header><p>Universities</p></Lesson_10Header>
+            <Lesson_10Cards></Lesson_10Cards>
+        </Lesson_10Div>
+    )
+}
+
+export default Lesson10;
